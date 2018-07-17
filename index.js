b335278ddc06b025449d750e4d3881563222a609
John Pena <jpena91@gmail.com>
2018-07-17 12:48:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 var name = "Joe"
 var height = "74"
-if (name === "Joey") {
-  console.log("This is the correct nombre hombre")
-} else {
-  console.log("Never disrespect Joe like that")
-}
 
 // Don't worry about this
 module.exports = { name, height
