4cfdcdec4807bb08ac29b7b05014124cedeb81cf
John Pena <jpena91@gmail.com>
2018-07-17 12:46:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var name = "Joe"
 var height = "74"
-if (name === "Joe") {
+if (name === "Joey") {
   console.log("This is the correct nombre hombre")
+} else {
+  console.log("Never disrespect Joe like that")
 }
 
 // Don't worry about this
